9999f7b328dcbae200e0ca71dc39e375b087597d
jzafvt771 <ijmm7171@gmail.com>
2024-04-03 17:24:05 -0500

    Component Extend Styles Pattern

--- src/patterns/components/ProductCard.tsx
+++ src/patterns/components/ProductCard.tsx
@@ -1,22 +1,29 @@
-import {createContext} from "react";
+import {ReactElement, createContext} from "react";
 
-import { ProductContextProps, ProductCardProps } from "../interfaces/product.interfaces";
+import { ProductContextProps, Product } from "../interfaces/product.interfaces";
 
 import styles from "../styles/styles.module.css";
 import useProduct from "../hooks/useProduct";
 
+
+export interface Props {
+    product: Product;
+    children?: ReactElement | ReactElement[];
+    className?:string;
+    style?: React.CSSProperties; 
+}
+
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
-export const ProductCard = ({ children, product }: ProductCardProps) => {
+export const ProductCard = ({ children, product, className, style }: Props) => {
     const { counter, handleCounter } = useProduct();
-
     return (
         <Provider value={{
             counter,
             handleCounter,
             product
         }}>
-            <div className={styles.productCard}>
+            <div className={`${styles.productCard} ${className}`} style={style}>
                 {children}
             </div>
         </Provider>
--- src/patterns/pages/ShoppingPage.tsx
+++ src/patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
+import '../styles/custom-styles.css'
 
 const product = {
     id: '1',
@@ -18,19 +19,39 @@ const ShoppingPage = () => {
                 flexWrap: "wrap"
             }
             }>
-                <ProductCard product={product}>
+                <ProductCard product={product} className="bg-dark">
                     {/* Modos de envio de Componentes Hijos -- Compound - Técnica 1 */}
-                    <ProductImage></ProductImage>
-                    <ProductTitle title={""}></ProductTitle>
-                    <ProductButtons></ProductButtons>
+                    <ProductImage className="custom-image"></ProductImage>
+                    <ProductTitle title={""} className="text-white text-bold"></ProductTitle>
+                    <ProductButtons className="custom-buttons text-white"></ProductButtons>
                 </ProductCard>
 
-                <ProductCard product={product}>
-                    {/* Modos de envio de Componentes Hijos -- Compound - Técnica 1 */}
-                    <ProductCard.Image></ProductCard.Image>
-                    <ProductCard.Title title={"Producto 2"}></ProductCard.Title>
-                    <ProductCard.Buttons></ProductCard.Buttons>
+                <ProductCard product={product} className="bg-dark">
+                    {/* Modos de envio de Componentes Hijos -- Compound - Técnica 2 */}
+                    <ProductCard.Image className="custom-image"></ProductCard.Image>
+                    <ProductCard.Title title={"Producto 2"} className="text-white text-bold"></ProductCard.Title>
+                    <ProductCard.Buttons className="custom-buttons text-white"></ProductCard.Buttons>
                 </ProductCard>
+
+                <ProductCard product={product} className="text-white" style={{
+                    backgroundColor: "#222ddd",
+                }}>
+                    {/* Modos de envio de Componentes Hijos -- Compound - Técnica 1 */}
+                    <ProductImage style={{
+                        opacity: 0.85
+                    }}></ProductImage>
+                    <ProductTitle style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        textAlign: "center",
+                    }} ></ProductTitle>
+                    <ProductButtons style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        textAlign: "center"
+                    }} className="custom-buttons text-white"></ProductButtons>
+                </ProductCard>
+
 
             </div>
         </div>
